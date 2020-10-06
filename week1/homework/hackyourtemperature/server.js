63c720e5f0461c56499e8952a0a140aeb17f6167
mervearas <mervegurzel@icloud.com>
2020-10-07 00:05:07 +0200

    homework and project done

--- week1/homework/hackyourtemperature/server.js
+++ week1/homework/hackyourtemperature/server.js
@@ -1,9 +1,25 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: false }));
 
 app.get('/', (req, res) => {
   res.send('hello from backend to frontend!')
+})
+
+app.post('/weather', (req, res) => {
+  const cityName = req.body.cityName;
+  if(typeof cityName === 'undefined') {
+    res.status(400);
+    res.end('Invalid Request');
+  }
+  res.end(cityName);
 })
 
 app.listen(port, () => {
--- week2/homework/exercises/1-blog-API/server.js
+++ week2/homework/exercises/1-blog-API/server.js
@@ -1,10 +1,78 @@
 const express = require('express')
 const app = express();
- 
+const fs = require("fs");
+app.use(express.json());
 
 // YOUR CODE GOES IN HERE
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.post('/blogs', (req, res) => {
+  if(isInvalid(req)) {
+    res.status(400);
+    res.send('Invalid request');
+    return;
+  }
+
+  const title = req.body.title;
+  const content = req.body.content;
+
+  fs.writeFileSync(title, content);
+  res.end('ok')
 })
- 
-app.listen(3000)
+
+app.put('/posts/:title', (req, res) => {
+  if(isInvalid(req)) {
+    res.status(400);
+    res.send('Invalid request');
+    return;
+  }
+
+  const title = req.params.title;
+  const content = req.body.content;
+
+  if (fs.existsSync(title)) {
+    fs.writeFileSync(title, content);
+    res.end('ok')
+  }
+  else {
+    // Send response with error message
+    res.status(404);
+    res.send('File does not exist!');
+  }
+})
+
+app.delete('/blogs/:title', (req, res) => {
+  // How to get the title from the url parameters?
+  const title = req.params.title;
+  if (fs.existsSync(title)) { // Add condition here
+    fs.unlinkSync(title);
+    res.end('ok');
+  } else {
+    // Respond with message here
+    res.status(404);
+    res.send('File does not exist!');
+  }
+})
+
+app.get('/blogs/:title', (req, res) => {
+  const title = req.params.title;
+  // How to get the title from the url parameters?
+  // check if post exists
+  if(fs.existsSync(title)) {
+    const post = fs.readFileSync(title);
+    res.send(post);
+  } else {
+    res.status(404);
+    res.send('File does not exist!');
+  }
+})
+
+function isInvalid(req) {
+  if(typeof req.body == 'undefined' ||
+     typeof req.body.title == 'undefined' ||
+     typeof req.body.content == 'undefined') {
+       return true
+  } else {
+    return false
+  }
+}
+
+app.listen(3000);
